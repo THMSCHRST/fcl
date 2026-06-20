e1b6ab6d9dadb931f888bd78e61d12a81ca6d310
THMSCHRST <die.ententaube@gmail.com>
2026-06-20 18:43:26 +0200

    commit

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fcl/td"
+	"fcl/w"
 	"runtime"
 
 	"github.com/go-gl/gl/v4.6-core/gl"
@@ -12,32 +14,7 @@ func init() {
 }
 
 func main() {
-    if err := glfw.Init(); err != nil {
-        panic(err)
-    }
-    defer glfw.Terminate()
-
-    // req opengl 4.6
-    glfw.WindowHint(glfw.ContextVersionMajor, 4)
-    glfw.WindowHint(glfw.ContextVersionMinor, 6)
-    glfw.WindowHint(glfw.OpenGLProfile, glfw.OpenGLCoreProfile)
-    glfw.WindowHint(glfw.OpenGLForwardCompatible, glfw.True)
-
-    window, err := glfw.CreateWindow(800, 600, "Hello OpenGL", nil, nil)
-    if err != nil {
-        panic(err)
-    }
-    window.MakeContextCurrent()
-
-    // init opengl
-    if err := gl.Init(); err != nil {
-        panic(err)
-    }
-
-    // set viewport
-    width, height := window.GetFramebufferSize()
-    gl.Viewport(0, 0, int32(width), int32(height))
-    gl.ClearColor(1, 1, 1, 1.0)
+	window := w.InitWindow(td.NewVec2(500,500),"Test")
 
     for !window.ShouldClose() {
         gl.Clear(gl.COLOR_BUFFER_BIT)
